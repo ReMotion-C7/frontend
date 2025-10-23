4bc7adc57619676e578c13be825c4563f48be622
Daniel Fernando Herawan <dfernando@student.ciputra.ac.id>
2025-10-23 08:44:05 +0700

    init

---
+++ ReMotion-FrontEnd/ReMotion-FrontEnd/ReMotion_FrontEndApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReMotion_FrontEndApp.swift
+//  ReMotion-FrontEnd
+//
+//  Created by Daniel Fernando Herawan on 23/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ReMotion_FrontEndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
