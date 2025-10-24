5ca854226293db2a9b60fa70dc5385651849d1a4
geboboi <146686496+geboboi@users.noreply.github.com>
2025-10-24 08:59:58 +0700

    slicing ui login register done

---
+++ ReMotion-FrontEnd/ReMotion-FrontEnd/App/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  ReMotion-FrontEnd
+//
+//  Created by Gabriela on 23/10/25.
+//
+
+import Foundation
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    static var orientationLock = UIInterfaceOrientationMask.landscapeLeft
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
+    }
+}
