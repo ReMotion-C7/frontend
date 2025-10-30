5927fc040202b56938eb338069dbe16c9474b617
yebology <139052533+yebology@users.noreply.github.com>
2025-10-30 11:45:00 +0700

    feat: done integrate read exercises on fisio

--- ReMotion-FrontEnd/ReMotion-FrontEnd/Features/DashboardFisio/View/LibraryGerakanPage.swift
+++ ReMotion-FrontEnd/ReMotion-FrontEnd/Features/DashboardFisio/View/LibraryGerakanPage.swift
@@ -8,28 +8,44 @@
 import SwiftUI
 
 struct LibraryGerakanPage: View {
+    
+    @StateObject private var viewModel = ExerciseViewModel()
+    
     var body: some View {
         GeometryReader { geometry in
-            ScrollView {
-                // Hitung jumlah kolom berdasarkan lebar layar
-                let availableWidth = geometry.size.width // padding
-                let itemWidth: CGFloat = 244
-                let spacing: CGFloat = 30
-                let columnsCount = max(Int(availableWidth / (itemWidth + spacing)), 1)
-                
-                // Buat grid layout dinamis
-                let columns = Array(repeating: GridItem(.flexible(), spacing: spacing), count: columnsCount)
-                
-                LazyVGrid(columns: columns, spacing: spacing) {
-                    ForEach(sampleMovements) { move in
-                        NavigationLink(destination: DetailMovementPage(movement: move)) {
-                            DashboardCardSmall(movement: move)
+            
+            if viewModel.isLoading {
+                LoadingView(message: "Memuat semua gerakan...")
+            } else if viewModel.errorMessage != "" {
+                ErrorView(message: "Gagal memuat gerakan latihan.")
+            }
+            else {
+                ScrollView {
+                    // Hitung jumlah kolom berdasarkan lebar layar
+                    let availableWidth = geometry.size.width // padding
+                    let itemWidth: CGFloat = 244
+                    let spacing: CGFloat = 30
+                    let columnsCount = max(Int(availableWidth / (itemWidth + spacing)), 1)
+                    
+                    // Buat grid layout dinamis
+                    let columns = Array(repeating: GridItem(.flexible(), spacing: spacing), count: columnsCount)
+                    
+                    LazyVGrid(columns: columns, spacing: spacing) {
+                        ForEach(viewModel.exercises) { move in
+                            NavigationLink(destination: DetailMovementPage(movement: move)) {
+                                DashboardCardSmall(movement: move)
+                            }
+                            .buttonStyle(PlainButtonStyle())
                         }
-                        .buttonStyle(PlainButtonStyle())
                     }
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 10)
                 }
-                .padding(.horizontal, 20)
-                .padding(.vertical, 10)
+            }
+        }
+        .onAppear {
+            Task {
+                try await viewModel.readExercises()
             }
         }
     }
