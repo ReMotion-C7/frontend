d7ab11b60318727a7de0f5f5804d33ea0a1173a7
Oxqlion <rafinaya1127@gmail.com>
2025-11-10 15:16:36 +0700

    feat: add conditional exercise and rest exercise sidebar

--- ReMotion-FrontEnd/ReMotion-FrontEnd/Features/Exercise/View/NewExerciseView.swift
+++ ReMotion-FrontEnd/ReMotion-FrontEnd/Features/Exercise/View/NewExerciseView.swift
@@ -12,81 +12,37 @@ import QuickPoseCore
 struct NewExerciseView: View {
     
     @Environment(\.dismiss) private var dismiss
+    @State var showExitModal: Bool = false
     @StateObject var viewModel = NewExerciseViewModel()
     
-    @State var showExitModal: Bool = false
+    let exercises: [NewExercises]
     
-    private var videoURL: String = "https://tjyoilicubnsdpujursp.supabase.co/storage/v1/object/public/ReMotion/Lunges%20Landscape.mp4"
+    private var currentVideoURL: URL? {
+        guard let currentPhase = viewModel.newCurrentPhase else { return nil }
+        
+        let urlString: String
+        switch currentPhase {
+        case .exercise(let details, _):
+            urlString = details.video
+        case .rest(_, let nextExercise):
+            urlString = nextExercise.video
+        }
+        
+        return URL(string: urlString)
+    }
     
     var body: some View {
         HStack {
-            VStack {
-                Text("Lunges")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                
-                Text("Otot Paha Depan")
-                    .padding(.horizontal, 16)
-                    .padding(.vertical, 10)
-                    .background(Color.black)
-                    .clipShape(Capsule())
-                    .font(.subheadline)
-                    .fontWeight(.medium)
-                    .foregroundStyle(Color.white)
-                
-                HStack {
-                    if let url = URL(string: videoURL) {
-                        VideoPlayerView(videoURL: url)
-                            .frame(maxWidth: 380, maxHeight: 300)
-                            .cornerRadius(16)
-                    }
+            
+            if let currentPhase = viewModel.newCurrentPhase {
+                switch currentPhase {
+                case .exercise(let details, let currentSet):
+                    ExercisePhaseSidebar(currentVideoURL: currentVideoURL, onNext: viewModel.newGoToNextPhase)
+                case .rest(let duration, let nextExercise):
+                    RestPhaseSidebar(onNext: viewModel.newGoToNextPhase)
                 }
-                .padding(.vertical, 24)
-                
-                Text("Set: 1/10")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                
-                Text("30x")
-                    .font(.system(size: 64))
-                    .fontWeight(.bold)
-                
-                Button(action: {
-                    print("Selanjutnya tapped")
-                }) {
-                    HStack {
-                        Image(systemName: "play.fill") // SF Symbol
-                        Text("Selanjutnya")
-                            .fontWeight(.semibold)
-                    }
-                    .foregroundColor(.white)
-                    .font(.subheadline)
-                    .frame(maxWidth: 320)
-                    .padding()
-                    .background(Color(.sRGB, white: 0.15, opacity: 1))
-                    .cornerRadius(16)
-                }
-                
-                Button(action: {
-                    print("Ulangi tapped")
-                }) {
-                    HStack {
-                        Image(systemName: "arrow.clockwise")
-                        Text("Ulangi")
-                            .fontWeight(.semibold)
-                    }
-                    .foregroundColor(.white)
-                    .font(.subheadline)
-                    .frame(maxWidth: 320)
-                    .padding()
-                    .background(Color(.sRGB, white: 0.15, opacity: 1))
-                    .cornerRadius(16)
-                }
-                
                 
             }
-            .frame(maxWidth: 400, maxHeight: .infinity)
-            .background(Color.white)
             
             ZStack {
                 //                Text("Ini bagian video bang")
@@ -136,7 +92,11 @@ struct NewExerciseView: View {
                 }
                 
             }
-            .onAppear(perform: viewModel.quickPoseSetup)
+            .allowsHitTesting(false)
+            .onAppear {
+                viewModel.quickPoseSetup()
+                viewModel.newStartSession(with: exercises)
+            }
             .onDisappear {
                 viewModel.quickPose.stop()
             }
@@ -159,5 +119,135 @@ struct NewExerciseView: View {
                 viewModel.showExitModal = false
             }
         }
+    }
+}
+
+struct RestPhaseSidebar: View {
+    
+    let onNext: () -> Void
+    
+    var body: some View {
+        VStack {
+            Text("Lunges")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Text("00:00:30")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Button(action: {
+                print("Selanjutnya tapped")
+                onNext()
+
+            }) {
+                HStack {
+                    Image(systemName: "play.fill") // SF Symbol
+                    Text("Selanjutnya")
+                        .fontWeight(.semibold)
+                }
+                .foregroundColor(.white)
+                .font(.subheadline)
+                .frame(maxWidth: 320)
+                .padding()
+                .background(Color(.sRGB, white: 0.15, opacity: 1))
+                .cornerRadius(16)
+            }
+            
+            Button(action: {
+                print("Ulangi tapped")
+            }) {
+                HStack {
+                    Image(systemName: "arrow.clockwise")
+                    Text("Ulangi")
+                        .fontWeight(.semibold)
+                }
+                .foregroundColor(.white)
+                .font(.subheadline)
+                .frame(maxWidth: 320)
+                .padding()
+                .background(Color(.sRGB, white: 0.15, opacity: 1))
+                .cornerRadius(16)
+            }
+        }
+        .frame(maxWidth: 400, maxHeight: .infinity)
+        .background(Color.white)
+    }
+}
+
+struct ExercisePhaseSidebar: View {
+    
+    let currentVideoURL: URL?
+    let onNext: () -> Void
+    
+    var body: some View {
+        VStack {
+            Text("Lunges")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Text("Otot Paha Depan")
+                .padding(.horizontal, 16)
+                .padding(.vertical, 10)
+                .background(Color.black)
+                .clipShape(Capsule())
+                .font(.subheadline)
+                .fontWeight(.medium)
+                .foregroundStyle(Color.white)
+            
+            HStack {
+                if let url = currentVideoURL {
+                    VideoPlayerView(videoURL: url)
+                        .frame(maxWidth: 380, maxHeight: 300)
+                        .cornerRadius(16)
+                }
+            }
+            .padding(.vertical, 24)
+            
+            Text("Set: 1/10")
+                .font(.title2)
+                .fontWeight(.semibold)
+            
+            Text("30x")
+                .font(.system(size: 64))
+                .fontWeight(.bold)
+            
+            Button(action: {
+                print("Selanjutnya tapped")
+                onNext()
+            }) {
+                HStack {
+                    Image(systemName: "play.fill") // SF Symbol
+                    Text("Selanjutnya")
+                        .fontWeight(.semibold)
+                }
+                .foregroundColor(.white)
+                .font(.subheadline)
+                .frame(maxWidth: 320)
+                .padding()
+                .background(Color(.sRGB, white: 0.15, opacity: 1))
+                .cornerRadius(16)
+            }
+            
+            Button(action: {
+                print("Ulangi tapped")
+            }) {
+                HStack {
+                    Image(systemName: "arrow.clockwise")
+                    Text("Ulangi")
+                        .fontWeight(.semibold)
+                }
+                .foregroundColor(.white)
+                .font(.subheadline)
+                .frame(maxWidth: 320)
+                .padding()
+                .background(Color(.sRGB, white: 0.15, opacity: 1))
+                .cornerRadius(16)
+            }
+            
+            
+        }
+        .frame(maxWidth: 400, maxHeight: .infinity)
+        .background(Color.white)
     }
 }
